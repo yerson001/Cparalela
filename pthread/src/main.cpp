cae5c1c1a22bcbb0050f5a00040d4f3b0dbb8075
Yerson sanchez <37232721+yerson001@users.noreply.github.com>
2021-10-05 17:26:04 -0500

    Add files via upload

--- pthread/src/main.cpp
+++ pthread/src/main.cpp
@@ -2,7 +2,15 @@
 #include <pthread.h>
 using namespace std;
 
+void print(int a){
+  cout<<"this->is a value"<<endl;
+  for(int i=0; i<3; i++){}
+}
+
 int main(){
   cout<<"hello pthread"<<endl;
+  for (int i = 0; i < 10; ++i) {
+   cout <<"test"<<i<<endl; 
+  }
   return 0;
 }
